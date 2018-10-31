91a3877d5d7d2a13f371f8327aae29fd76a4e4c9
Melissa-Struys <36380857+Melissa-Struys@users.noreply.github.com>
2018-10-31 13:18:25 +0100

    Add files via upload

---
+++ IV.py
@@ -0,0 +1,34 @@
+import openpyxl
+import sys
+from openpyxl.utils import get_column_letter, column_index_from_string
+
+class IV():
+    wb = openpyxl.load_workbook('__PID_BIFI_NPERT_JW_5BB.xlsx', data_only=True)
+    sheet = wb['JW1_F']
+
+    def getIVlist(filename):
+        with open(filename, 'r') as file:
+            wholeFile = file.read()
+            data = wholeFile.split('**Data**')[1]
+            #print(data)
+
+            splitted = data.split()
+
+            v = []
+            i = []
+
+            if str(filename).endswith('drk'):
+                jump = 3
+            else :
+                jump = 4
+
+            for j in range(0, len(splitted),jump):
+                v.append(splitted[j])
+                i.append(splitted[j+1])
+
+            #print(v)
+            #print(i)
+
+            iv = [v, i]
+
+            return iv
---
+++ Main.py
@@ -0,0 +1,11 @@
+import IV
+
+class Main():
+    vDark = IV.IV.getIVlist('JW1_F.drk')[0]
+    iDark = IV.IV.getIVlist('JW1_F.drk')[1]
+    vLight = IV.IV.getIVlist('JW1_F.lgt')[0]
+    iLight = IV.IV.getIVlist('JW1_F.lgt')[1]
+    print(vDark)
+    print(iDark)
+    print(vLight)
+    print(iLight)

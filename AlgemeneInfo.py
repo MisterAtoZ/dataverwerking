02c0430fbd54e84a89253ef3b8f615dd9cae9242
Melissa Struys <melissa@student>
2018-11-04 15:44:18 +0100

    uren berekenen

--- AlgemeneInfo.py
+++ AlgemeneInfo.py
@@ -9,13 +9,33 @@ class AlgemeneInfo():
         data = dataEx['data-exchange']
         generalSheet = wb['General']
 
-        aantalMetingen = 0
-        for i in range(19, generalSheet.max_row, 1):
-            if generalSheet.cell(row=i, column=5).value == None:
+        # uren berekenen
+        vorigUur = 0
+        uren = [0]
+        for i in range(20, generalSheet.max_row, 1):
+            if generalSheet.cell(row=i, column=1).value == None:
                 break
             else:
-                aantalMetingen = aantalMetingen + 1
-        print('aantalMetingen ' + str(aantalMetingen))
+                date1 = generalSheet.cell(row=i, column=1).value
+                print(date1)
+                date2 = generalSheet.cell(row=i - 1, column=1).value
+                timeOut = generalSheet.cell(row=i, column=2).value
+                timeIn = generalSheet.cell(row=i - 1, column=3).value
+                print(timeOut)
+
+                date1 = date1.replace(hour=timeOut.hour, minute=timeOut.minute)
+                print(date1)
+                date2 = date2.replace(hour=timeIn.hour, minute=timeIn.minute)
+                print(date2)
+
+                seconds = (date1 - date2).total_seconds()
+                interval = seconds / 3600
+
+                generalSheet.cell(row=i, column=4).value = interval
+                generalSheet.cell(row=i, column=5).value = interval + vorigUur
+
+                vorigUur = generalSheet.cell(row=i, column=5).value
+                uren.append(vorigUur)
 
         for n in range(0, 4, 1):
             if (n == 0):
@@ -27,22 +47,25 @@ class AlgemeneInfo():
             else:
                 sheet = wb['JW2_F']
 
+            #print('----NEXT ROW----')
             nextRow = 0
             for cellObj in sheet['A']:
                 if cellObj.value == None:
                     nextRow = cellObj.row
                     break
-            # print('nextRow ' + str(nextRow))
+            #print('nextRow ' + str(nextRow))
             begin = nextRow
 
-            uren = []
 
-            for rows in range(begin, 2+aantalMetingen, 1):
-                sheet.cell(row=nextRow, column=1).value = round(float(generalSheet.cell(row=17+nextRow, column=5).value),2)
+            #print('----INVULLEN----')
+            for rows in range(begin, 2+len(uren), 1):
+                print(str(len(uren)))
+                uurInt = int(round(uren[rows-2],0))
+                uurAfgerond = round(uren[rows-2],2)
+                #print('uren[' + str(rows) + '] ' + str(uur))
+                sheet.cell(row=nextRow, column=1).value = uurAfgerond
 
-                print(str(sheet.cell(row=nextRow, column=1).value) + " " +str(nextRow))
-                #print(sheet)
-                #print(sheet==wb['JW1_F'])
+                #print(str(sheet.cell(row=nextRow, column=1).value) + " " +str(nextRow))
 
                 if sheet == wb['JW2_B']:
                     staal_nr = 4
@@ -54,22 +77,20 @@ class AlgemeneInfo():
                     staal_nr = 7
                 #print(staal_nr)
 
-                uur = int(round(float(generalSheet.cell(row=17+nextRow, column=5).value),0))
-                print(uur)
-
-                uren.append(uur)
-
                 for i in range(staal_nr, data.max_row, 4):
-                    print(str(data.cell(row=i, column=1).value) + " " + str(uur))
-                    if(str(data.cell(row=i, column=1).value) == str(uur)):
+                    #print(str(data.cell(row=i, column=1).value) + " " + str(uurInt))
+                    if(str(data.cell(row=i, column=1).value) == str(uurInt)):
                         for j in range(2, 13, 1):
                             sheet.cell(row=nextRow, column=j).value = data.cell(row=i, column=j+7).value
-                            print(sheet.cell(row=nextRow, column=j).value)
+                            #print(sheet.cell(row=nextRow, column=j).value)
                         break
 
                 nextRow = nextRow + 1
-                print('-----------------------')
+                #print('-----------------------')
 
-        #print('Saving...')
+        print('Saving...')
         wb.save('__PID_BIFI_NPERT_JW_5BB_updated.xlsx')
+        for i in range(0, len(uren),1):
+            uren[i] = int(round(uren[i],0))
+
         return uren
--- Main.py
+++ Main.py
@@ -2,76 +2,75 @@ import AlgemeneInfo
 import IV
 import openpyxl
 import os
-from os import listdir
-from os.path import isfile, join
-from openpyxl.utils import get_column_letter, column_index_from_string
 
 class Main():
     uren = AlgemeneInfo.AlgemeneInfo.datasheet()
+
     wb = openpyxl.load_workbook('__PID_BIFI_NPERT_JW_5BB_updated.xlsx', data_only=True)
     generalSheet = wb['General']
 
     thisdir = os.getcwd()
     print(os.listdir())
     print(os.listdir(thisdir + '/' + str(752)))
+    for uur in range(6, len(uren),1):
+        for n in range(0, 4, 1):
+            if (n == 0):
+                sheet = 'JW1_B'
+                nieuwPad = './'+str(uren[uur])+'/JW1_B'
+            elif (n == 1):
+                sheet = 'JW1_F'
+                nieuwPad = './' + str(uren[uur]) + '/JW1_F'
+            elif (n == 2):
+                sheet = 'JW2_B'
+                nieuwPad = './' + str(uren[uur]) + '/JW2_B'
+            else:
+                sheet = 'JW2_F'
+                nieuwPad = './' + str(uren[uur]) + '/JW2_F'
 
-    for n in range(0, 4, 1):
-        if (n == 0):
-            sheet = 'JW1_B'
-            nieuwPad = './'+str(752)+'/JW1_B'
-        elif (n == 1):
-            sheet = 'JW1_F'
-            nieuwPad = './' + str(752) + '/JW1_F'
-        elif (n == 2):
-            sheet = 'JW2_B'
-            nieuwPad = './' + str(752) + '/JW2_B'
-        else:
-            sheet = 'JW2_F'
-            nieuwPad = './' + str(752) + '/JW2_F'
+            ivPad = nieuwPad + '/IV/' + sheet
+            drk = IV.IV.getIVlist(str(ivPad) + '.drk', sheet)
+            lgt = IV.IV.getIVlist(str(ivPad) + '.lgt', sheet)
 
-        ivPad = nieuwPad + '/IV/' + sheet
-        drk = IV.IV.getIVlist(str(ivPad) + '.drk', sheet)
-        lgt = IV.IV.getIVlist(str(ivPad) + '.lgt', sheet)
+            vDark = drk[0]
+            iDark = drk[1]
+            vLight = lgt[0]
+            iLight = lgt[1]
 
-        vDark = drk[0]
-        iDark = drk[1]
-        vLight = lgt[0]
-        iLight = lgt[1]
+            activeSheet = wb[sheet]
 
-        activeSheet = wb[sheet]
-
-        #uur
-        column1 = activeSheet.max_column + 2
-        column2 = activeSheet.max_column + 5
-        activeSheet.merge_cells(start_row = 1, start_column = column1, end_row = 1, end_column = column2)
-        activeSheet.cell(row=1, column=column1).value = str(uren[0]) + ' h'
-
-
-        #light dark
-        column3 = column1 + 1
-        activeSheet.merge_cells(start_row = 2, start_column = column1, end_row = 2, end_column = column3)
-        activeSheet.cell(row=2, column=column1).value = 'light'
-
-        column4 = column1 + 2
-        column5 = column4 + 1
-        activeSheet.merge_cells(start_row = 2, start_column = column4, end_row = 2, end_column = column5)
-        activeSheet.cell(row=2, column=column4).value = 'dark'
+            #uur
+            column1 = activeSheet.max_column + 2
+            column2 = activeSheet.max_column + 5
+            activeSheet.merge_cells(start_row = 1, start_column = column1, end_row = 1, end_column = column2)
+            activeSheet.cell(row=1, column=column1).value = str(uren[uur]) + ' h'
 
 
-        #iv invullen
-        activeSheet.cell(row=3, column=column1).value = 'V'
-        activeSheet.cell(row=3, column=column3).value = 'I'
-        activeSheet.cell(row=3, column=column4).value = 'V'
-        activeSheet.cell(row=3, column=column5).value = 'I'
+            #light dark
+            column3 = column1 + 1
+            activeSheet.merge_cells(start_row = 2, start_column = column1, end_row = 2, end_column = column3)
+            activeSheet.cell(row=2, column=column1).value = 'light'
 
-        #data invullen
-        for j in range(0,len(vDark),1):
-            activeSheet.cell(row=j+4, column=column1).value = vLight[j]
-            activeSheet.cell(row=j+4, column=column3).value = iLight[j]
-            activeSheet.cell(row=j+4, column=column4).value = vDark[j]
-            activeSheet.cell(row=j+4, column=column5).value = iDark[j]
+            column4 = column1 + 2
+            column5 = column4 + 1
+            activeSheet.merge_cells(start_row = 2, start_column = column4, end_row = 2, end_column = column5)
+            activeSheet.cell(row=2, column=column4).value = 'dark'
+
+
+            #iv invullen
+            activeSheet.cell(row=3, column=column1).value = 'V'
+            activeSheet.cell(row=3, column=column3).value = 'I'
+            activeSheet.cell(row=3, column=column4).value = 'V'
+            activeSheet.cell(row=3, column=column5).value = 'I'
+
+            #data invullen
+            for j in range(0,len(vDark)-1,1):
+                activeSheet.cell(row=j+4, column=column1).value = vLight[j]
+                activeSheet.cell(row=j+4, column=column3).value = iLight[j]
+                activeSheet.cell(row=j+4, column=column4).value = vDark[j]
+                activeSheet.cell(row=j+4, column=column5).value = iDark[j]
 
 
     print('Saving...')
     wb.save('__PID_BIFI_NPERT_JW_5BB_updated.xlsx')
+
 
